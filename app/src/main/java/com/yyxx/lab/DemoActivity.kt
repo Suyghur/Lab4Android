ad3b7152e041f3e5b6e56da94a9fde92e16b7a74
#Suyghur <Suyghurmjp@outlook.com>
2022-01-10 20:02:15 +0800

    仓库初始化

--- app/src/main/java/com/yyxx/lab/DemoActivity.kt
+++ app/src/main/java/com/yyxx/lab/DemoActivity.kt
@@ -1,9 +1,14 @@
 package com.yyxx.lab
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.yyxx.lab.ui.theme.Lab4AndroidTheme
 
 /**
  * @author #Suyghur.
@@ -13,6 +18,27 @@ class DemoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.setBackgroundDrawable(ColorDrawable(Color.WHITE))
+        setContent {
+            Lab4AndroidTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    DemoPage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DemoPage() {
+    Text(text = "哈哈")
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Lab4AndroidTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            DemoPage()
+        }
     }
 }
