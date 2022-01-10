fceee5280728e03ff4efb536bb5025a4de505dea
#Suyghur <Suyghurmjp@outlook.com>
2022-01-10 20:42:56 +0800

    v0.0.1开发：界面UI开发

--- app/src/main/java/com/yyxx/lab/DemoActivity.kt
+++ app/src/main/java/com/yyxx/lab/DemoActivity.kt
@@ -1,13 +1,22 @@
 package com.yyxx.lab
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.yyxx.lab.ui.theme.Lab4AndroidTheme
 
 /**
@@ -30,7 +39,20 @@ class DemoActivity : AppCompatActivity() {
 
 @Composable
 fun DemoPage() {
-    Text(text = "哈哈")
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Button(modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp), onClick = {
+            Toast.makeText(context, "测试Toast", Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "哈哈")
+        }
+    }
 }
 
 @Preview(showSystemUi = true, showBackground = true)
